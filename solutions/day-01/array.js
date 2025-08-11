fa5c377459d23e71f584096910f4aa544e0a35f4
Fauzan Rhamzy <fauzanrhamzy04@gmail.com>
2025-08-11 23:09:05 +0700

    chore: exercise level1 not complete

---
+++ solutions/day-01/array.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5, 6]
+console.log(numbers.splice(3, 3, 7, 8, 9)) // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
+console.log(numbers) // -> [1, 2, 3, 7, 8, 9] //it removes three item and replace three items
---
+++ solutions/day-01/level1.js
@@ -0,0 +1,66 @@
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+]
+
+// 1
+const emptyArr = []
+
+// 2
+const arr5 = [1,2,3,4,5,6]
+
+// 3
+console.log("Length:", arr5.length);
+
+// 4
+console.log("First item:", arr5[0]);
+
+// 5
+const mixedDataTypes = [1, "two", 3, "four", 5.1, "six", 7.2]
+console.log("mixedDataTypes length:", mixedDataTypes.length);
+
+// 6
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+
+// 7
+console.log("companies:", itCompanies);
+
+// 8
+console.log("number of companies:", itCompanies.length);
+
+// 9
+console.log("first company:", itCompanies[0]);
+console.log("second company:", itCompanies[parseInt((itCompanies.length)/2)]);
+console.log("last company:", itCompanies[itCompanies.length-1]);
+
+// 10
+itCompanies.forEach(element => {
+    console.log(element);
+});
+
+// 11
+for (let index = 0; index < itCompanies.length; index++) {
+    itCompanies[index] = itCompanies[index].toUpperCase()
+    console.log(itCompanies[index]);
+}
+
+// 12
