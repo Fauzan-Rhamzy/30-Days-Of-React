0f6ae57f3cfe4b8d9d13738689be47380cf810ed
Fauzan Rhamzy <fauzanrhamzy04@gmail.com>
2025-08-13 07:45:10 +0700

    loop exercise

---
+++ solutions/day-01/Array-of-arrays/level2.js
@@ -0,0 +1,1 @@
+console.log(webTechs.toString());
---
+++ solutions/day-01/ternary-operators/level1.js
@@ -0,0 +1,20 @@
+// 1
+// let age = prompt("Enter your age: ")
+// if (age >= 18) {
+//     console.log("You are old enough to drive.");
+// } else {
+//     console.log("You are left with", (18-age), "years to drive.");
+// }
+
+// 2
+// let age = prompt("Enter your age: ")
+
+// 3
+// let a = 4, b = 3
+// if (a > b) {
+//     console.log("a is greater than b");
+// } else {
+//     console.log("a is less than b");
+// }
+
+// 4
